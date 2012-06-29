e0c5ef7ad681d535800633a341161985996187d7
Nate Abele <nate.abele@gmail.com>
2012-06-29 02:52:38 -0400

    Adding test command.

--- scripts/new.sh
+++ scripts/new.sh
@@ -12,6 +12,7 @@ mkdir ./_build/manifests 2> /dev/null
 cp $DIR/resources/manifests/default.conf ./_build/manifests/default.conf 2> /dev/null
 cp $DIR/resources/manifests/default.pp ./_build/manifests/default.pp 2> /dev/null
 cp $DIR/resources/manifests/php.ini ./_build/manifests/php.ini 2> /dev/null
+cp $DIR/resources/test ./_build/test 2> /dev/null
 
 touch `pwd`/.gitignore 2> /dev/null
 
