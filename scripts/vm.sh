3216d62eeab7e7fdfe3b6e7b8037f64fdb687968
Nate Abele <nate.abele@gmail.com>
2012-08-26 00:36:44 -0400

    Implementing automatic installation of virtual machine components.

--- scripts/vm.sh
+++ scripts/vm.sh
@@ -1,37 +1,44 @@
 #!/bin/bash
 
-if [[ "$(which vagrant)" != "" && "$(which VBoxManage)" != "" ]]; then
-	echo "Passed."
-
-	echo ""
-	echo "Checking for base box..."
-
-	if [[ "$(vagrant box list)" =~ lucid64 ]]; then
-		echo "Base box installed."
-	else
-		echo "Not found. Installing base box lucid64..."
-		vagrant box add lucid64 http://files.vagrantup.com/lucid64.box
-	fi
-	if [[ "$(vagrant box list)" =~ lucid64 ]]; then
-		echo ""
-		echo "You may start the project by running \`vagrant up\` and browsing to http://localhost:8081 (port mapping can be changed in ./Vagrantfile)."
-
-		echo ""
-		read -rsn 1 -p "Press enter to launch the project now..." < /dev/tty
-		echo ""
-
-		vagrant destroy -f > /dev/null
-		vagrant up
-		open "http://localhost:8081"
-	fi
-fi
+echo "Checking virtual machine system..."
 echo ""
 
+if [[ "$(which vagrant)" != "" && "$(which VBoxManage)" != "" ]]; then
+	echo "Passed."
+else
+	echo "One or more required virtual machine components not installed."
+	read -rsn 1 -p "Press enter to install now..." < /dev/tty
+fi
+
 if [ "$(which VBoxManage)" == "" ]; then
-	echo "VirtualBox not installed. Grab the latest version from https://www.virtualbox.org/wiki/Downloads"
+	echo "VirtualBox not installed, downloading now...";
+	curl --progress-bar http://dlc.sun.com.edgesuite.net/virtualbox/4.1.20/VirtualBox-4.1.20-80170-OSX.dmg -o /tmp/VirtualBox.dmg
+	echo "Installing..."
+	hdiutil attach /tmp/VirtualBox.dmg > /dev/null
+	sudo installer -pkg /Volumes/VirtualBox/VirtualBox.mpkg -target / > /dev/null
+	hdiutil eject /Volumes/VirtualBox > /dev/null
+	echo "Done."
 fi
 
 if [ "$(which vagrant)" == "" ]; then
-	echo "Vagrant not installed. Grab the latest version from http://downloads.vagrantup.com/"
+	echo "Vagrant not installed, downloading now...";
+	curl --progress-bar http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/Vagrant-1.0.3.dmg -o /tmp/Vagrant.dmg
+	echo "Installing..."
+	hdiutil attach /tmp/Vagrant.dmg > /dev/null
+	sudo installer -pkg /Volumes/Vagrant/Vagrant.pkg -target / > /dev/null
+	hdiutil eject /Volumes/Vagrant > /dev/null
+	echo "Done."
 fi
+
+echo ""
+echo "You may start the project by running \`vagrant up\` and browsing to http://localhost:8081 (port mapping can be changed in ./Vagrantfile)."
+
+echo ""
+read -rsn 1 -p "Press enter to launch the project now..." < /dev/tty
+echo ""
+
+vagrant destroy -f > /dev/null
+vagrant up
+open "http://localhost:8081"
+
 echo ""
